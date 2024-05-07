ad3da480547766f659130b09bb8dd43d4ee999b0
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-05-07 11:25:26 +0200

    Update DbInterface query method

--- Support/Contracts/DbInterface.php
+++ Support/Contracts/DbInterface.php
@@ -4,7 +4,7 @@ namespace Support\Contracts;
 
 interface DBInterface {
     public static function connect();
-    public static function query($query);
+    public static function query($query, $params);
     public function close();
     public static function getInstance();
 }
