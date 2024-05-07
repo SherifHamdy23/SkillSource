78ad64403aac4e52c1021d4cd8e200d19560531b
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-05-07 11:30:15 +0200

    add routes for the authentication

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@
 
     use Support\Router;
     use Controllers\AboutController;
-    use Controllers\UsersController;
+use Controllers\AuthController;
+use Controllers\UsersController;
 
     // Example usage
     // Define routes
@@ -12,11 +13,17 @@
         echo "Hello, world!";
     });
     Router::get('/users', [UsersController::class, 'index']);
+    Router::get('/login', function () {
+        view('login');
+    });
+    Router::post('/login', [AuthController::class, 'login']);
     Router::get('/about', [AboutController::class, 'index']);
+    Router::get('/home', fn () => view('home'));
+    Router::get('/logout', [AuthController::class, 'logout']);
     Router::get('/test', function () {
         echo "Hello Gentle men";
     });
     // Dispatch the request
     $requestUrl = $_SERVER['REQUEST_URI'];
-    echo $_SERVER['REQUEST_METHOD']."<br>";
+    // echo $_SERVER['REQUEST_METHOD']."<br>";
     Router::dispatch($requestUrl, $_SERVER['REQUEST_METHOD']);
