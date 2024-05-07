c0d15a64f58340fd271a2a2854eb828270407d2e
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-05-07 12:11:54 +0200

    add setup directory & sql_setup file

---
+++ setup/setup_db.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS pp_uni;
+
+use pp_uni;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    account_type VARCHAR(255) NOT NULL DEFAULT 'jobseeker',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE jobs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recuiter_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    requirments TEXT NOT NULL,
+    salary int NOT NULL,
+    location VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (recuiter_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS applications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    job_id INT NOT NULL,
+    job_seeker_id INT NOT NULL,
+    resume VARCHAR(255) NOT NULL,
+    cover_letter TEXT NOT NULL,
+    status VARCHAR(100) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (job_id) REFERENCES jobs(id),
+    FOREIGN KEY (job_seeker_id) REFERENCES users(id)
+);
+
+
