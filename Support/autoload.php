368058df16339a10e2de7e465c623e8752500905
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-06-21 11:34:55 +0200

    update

--- Support/autoload.php
+++ Support/autoload.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function ($class_name) {
     require_once __DIR__.'/helper/functions.php';
-    $base_directory = "/../"; // Base directory relative to the current directory
+    $base_directory = ""; // Empty string to load from root directory or its subdirectories
     $file_extension = '.php'; // File extension of PHP files
 
     // Convert namespace separators (\) to directory separators (/)
@@ -10,8 +10,8 @@ spl_autoload_register(function ($class_name) {
     // Define potential paths to check for the file
     $potential_paths = [
         __DIR__."/". $class_file,     // Check in current directory
-        __DIR__.$base_directory . $class_file, // Check in parent directory
-        __DIR__.$base_directory . str_replace('/', DIRECTORY_SEPARATOR, $class_file) // Check in subdirectories of parent directory
+        __DIR__.$base_directory . $class_file, // Check in root directory
+        __DIR__.$base_directory . str_replace('/', DIRECTORY_SEPARATOR, $class_file) // Check in subdirectories of root directory
     ];
 
     // Attempt to include the class file from the first valid path
