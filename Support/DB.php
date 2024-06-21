452b5f09c222759e65c6cffc971aff0c70fbc066
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-06-21 10:42:07 +0200

    update to use external database server

--- Support/DB.php
+++ Support/DB.php
@@ -15,25 +15,31 @@ class DB implements DBInterface {
     }
 
     public static function connect() {
-            $host = '127.0.0.1'; // Your host name
-            $dbname = 'pp_uni'; // Your database name
-            $username = 'root'; // Your database username
-            $password = ''; // Your database password
+        $uri = "mysql://avnadmin:AVNS_rlxpS4nUoHpYFHrZiMq@mysql-251a5251-sherifhamdy-09b8.f.aivencloud.com:14471/defaultdb?ssl-mode=REQUIRED";
 
-            try {
-                // Create a PDO instance
-                $pdo = new \PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-                static::$instance = $pdo;
-                
-                // Set PDO to throw exceptions on errors
-                $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                
-                
-                // echo "Connected successfully";
-            } catch (\PDOException $e) {
-                // If connection fails, handle the exception
-                echo "Connection failed: " . $e->getMessage();
-            }
+        $fields = parse_url($uri);
+        
+        // build the DSN including SSL settings
+        $conn = "mysql:";
+        $conn .= "host=" . $fields["host"];
+        $conn .= ";port=" . $fields["port"];;
+        $conn .= ";dbname=defaultdb";
+        $conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+        try {
+            // Create a PDO instance
+            $db = new \PDO($conn, $fields["user"], $fields["pass"]);
+            static::$instance = $db;
+            
+            // Set PDO to throw exceptions on errors
+            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            
+            
+            // echo "Connected successfully";
+        } catch (\PDOException $e) {
+            // If connection fails, handle the exception
+            echo "Connection failed: " . $e->getMessage();
+        }
     }
 
     public static function query($query, $params = []) {
--- setup/setup_db.sql
+++ setup/setup_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
-    account_type VARCHAR(255) NOT NULL DEFAULT 'jobseeker',
+    account_type ENUM('jobseeker', 'Recuiter') NOT NULL DEFAULT 'jobseeker',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
