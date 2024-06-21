a41d689676d5bf89d7d1a4c0491f3b644bab5f1f
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-06-21 11:49:25 +0200

    autoloader was fine & DBInterface with DbInterface which caused the Error in loading

--- Support/autoload.php
+++ Support/autoload.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function ($class_name) {
     require_once __DIR__.'/helper/functions.php';
-    $base_directory = "/../"; // Empty string to load from root directory or its subdirectories
+    $base_directory = '/../'; // Base directory relative to the current directory
     $file_extension = '.php'; // File extension of PHP files
 
     // Convert namespace separators (\) to directory separators (/)
@@ -10,14 +10,12 @@ spl_autoload_register(function ($class_name) {
     // Define potential paths to check for the file
     $potential_paths = [
         __DIR__."/". $class_file,     // Check in current directory
-        __DIR__.$base_directory . $class_file, // Check in root directory
-        __DIR__.$base_directory . str_replace('/', DIRECTORY_SEPARATOR, $class_file) // Check in subdirectories of root directory
+        __DIR__.$base_directory . $class_file // Check in parent directory
     ];
 
     // Attempt to include the class file from the first valid path
     foreach ($potential_paths as $path) {
         if (file_exists($path)) {
-            echo $path."<br>";
             include $path;
             return;
         }
