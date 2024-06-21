15679a63a867d85bb1006a4132fa9e6818a4c6ad
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-06-21 10:20:30 +0200

    updated

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../Support/autoload.php');
+    require_once(__DIR__.'/../Support/autoload.php');
 
     use Support\Router;
     use Controllers\Auth\AuthController;
@@ -11,4 +11,4 @@
     Router::post('/register', [AuthController::class, 'register']);
     Router::get('/logout', [AuthController::class, 'logout']);
     Router::get('/profile', fn() => view('auth/profile'));
-    require_once 'api.php';
+    require_once __DIR__.'/api.php';
